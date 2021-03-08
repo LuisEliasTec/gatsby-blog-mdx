a259c9f5cc2f385876b74aa3e4861024ef9d3a8d
Luis Elías <luiselias_1@hotmail.com>
2021-03-07 17:20:28 -0700

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import SocialLinks from '../constants/socialLinks'
 const Footer = () => {
-  return <h2>footer component</h2>
+  return <Footer className="footer">
+    <div>
+      <SocialLinks styleClass="footer-icons"/>
+      <p>
+        &copy;{new Date().getFullYear()} MDXBlog, All rights reserved
+      </p>
+    </div>
+  </Footer>
 }
 
 export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+ import React from 'react'
 import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
@@ -6,7 +6,9 @@ import { graphql } from 'gatsby'
 import SEO from '../components/SEO'
 // ...GatsbyImageSharpFluid
 const IndexPage = () => {
-  return <h2>home page</h2>
+  return <Layout>
+    <Hero showPerson/>
+  </Layout>
 }
 
 export default IndexPage
